4273e44396feb630524ae181af11f6e807049fdb
NikolaMrkic <nikola.mrkic93@gmail.com>
2020-01-12 18:10:17 +0100

    first commit

---
+++ src/main/java/com/hairdressingmanagementsistem/HairdressingCoreManagementSistemApplication.java
@@ -0,0 +1,14 @@
+package com.hairdressingmanagementsistem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairdressingCoreManagementSistemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairdressingCoreManagementSistemApplication.class, args);
+		System.out.println("Test");
+	}
+
+}
---
+++ src/test/java/com/hairdressingmanagementsistem/HairdressingCoreManagementSistemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hairdressingmanagementsistem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairdressingCoreManagementSistemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
